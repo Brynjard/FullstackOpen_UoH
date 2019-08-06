704c5db0f6d5519618a01a551277c1880004c18d
Brynjard <brynjardbm@gmail.com>
2019-08-06 21:06:43 +0200

    Practicing with state and eventhandling

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -1,79 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+
+//Html element displaying the counter:
+const Display = ({counter}) => <div>{counter}</div>
+
+const Button = ({onClick, text}) => (
+  <button onClick = {onClick}>
+    {text}
+  </button>
+);
 
 
-const Header = (props) => {
-  return (
-    <h1>{props.course}</h1>
-  );
-}
 
-const Part = (props) => {
-  return (
-    <p>
-    {props.part} {props.exercises}
-    </p>
-  );
-}
+const App = (props) => {
+  const [ counter, setCounter ] = useState(0);
 
-const Content = (props) => {
-  return (
-    <div>
-    <p>
-    {props.part1} {props.exercises1}
-    </p>
-    <p>
-    {props.part2} {props.exercises2}
-    </p>
-    <p>
-    {props.part3} {props.exercises3}
-    </p>
-    </div>
-  );
-}
-
-const Total = (props) => {
-  return(
-    <p>Number of exercises {props.course1 + props.course2 + props.course3}</p>
-  );
-}
-
-
-const App = () => {
-  const course = {
-      
-      name : 'Halfstack application development', 
-      
-      parts : [{
-        name : "Fundamentals of react",
-        exercises : 10
-      },
-               
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-               
-      {
-        name: 'State of a component',
-        exercises: 14
-      }]
-      
-  };
-
+  const setToValue = (value) => setCounter(value);
 
   return(
     <div>
-    <Header course = {course.name} />
-    <Content part1 = {course.parts[0].name} exercises1 = {course.parts[0].exercises}
-            part2 = {course.parts[1].name} exercises2 ={course.parts[1].exercises}
-            part3 = {course.parts[2].name} exercises3 ={course.parts[2].exercises} />
-    <Total course1 = {course.parts[0].exercises} course2 = {course.parts[1].exercises} course3 = {course.parts[2].exercises} />
+      <Display counter = {counter}/>
+      <Button 
+        onClick = {() => setToValue(counter + 1)} 
+        text = 'plus'
+      />
+      <Button onClick = {() => setToValue(counter - 1)}
+      text = 'minus'
+      />
+      <Button
+        onClick = {() => setToValue(0)}
+        text = 'zero'
+      />
     </div>
   );
-}
+};
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-
-//Nettopp ferdig med 1.4
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
