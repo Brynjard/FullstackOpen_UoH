33dca23b10c9980140259e775eae2205b1acaae4
Brynjard <brynjardbm@gmail.com>
2019-08-07 20:06:49 +0200

    Finished 1.6

---
+++ part1/unicafe/src/index.js
@@ -0,0 +1,36 @@
+import React, {useState} from 'react';
+import ReactDOM from 'react-dom';
+
+
+const Button = ({clickHandler, text}) => {
+    return (
+        <button onClick = {clickHandler}>
+            {text}
+        </button>
+        );
+}
+
+const App = () => {
+    //save clicks of each button to own state
+    const [good, setGood] = useState(0);
+    const [neutral, setNeutral] = useState(0);
+    const [bad, setBad] = useState(0);
+
+    return (
+        <div>
+            <h1>Give feedback</h1>
+            <Button text = "good" clickHandler = {() => {setGood(good + 1)}}/>
+            <Button text = "neutral" clickHandler = {() => {setNeutral(neutral + 1)}}/>
+            <Button text = "bad" clickHandler = {() => {setBad(bad + 1)}}/>
+            <h1>Statistics</h1>
+            <h4>Good: {good}</h4>
+            <h4>Neutral: {neutral}</h4>
+            <h4>Bad: {bad}</h4>
+        </div>
+    );
+}
+
+
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
